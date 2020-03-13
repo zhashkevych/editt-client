7a657f16aae9d5756a5310822220d27a44e7ac2c
Max Zhashkevych <maxzhashkevych@Maxs-MacBook-Air.local>
2020-03-13 10:42:16 +0200

    publication changes

--- src/components/Publication.vue
+++ src/components/Publication.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="publication">
-        <img src="https://miro.medium.com/max/4700/0*2gNPh0hZt_POaNlN" alt="">
-        <h2 class="heading">Путь к Простоте</h2>
-
+        <div class="croped-image">
+            <img src="https://miro.medium.com/max/4700/0*2gNPh0hZt_POaNlN" alt="">
+        </div>
+        
+        <div class="heading">
+            <p class="hashtags">#мінімалізм #лайфстайл #таймменеджмент</p>
+            <h2 class="heading">Путь к Простоте</h2>
+        </div>
+    
         <div class="bottom">
             <div class="left">
                 <p class="author">Від Василь Пупкін</p>
                 <p class="reading-time">Час читання: 4хв</p>
-            </div>
-            
-            <div class="right">
-                <p class="hashtags">#мінімалізм #лайфстайл #таймменеджмент</p>
             </div>
         </div>
        
@@ -22,13 +24,24 @@
         position: relative;
     }
 
-    .publication img {
+    .croped-image {
         width: 95%;
+        height: 10rem;
+        overflow: hidden;
+    }
+    .publication img {
+       width: 340px;
+       height: 160px;
     }
 
     .heading {
         font-size: 24px;
         margin-bottom: 8rem;
+        margin-top: 0;
+    }
+    
+    .hashtags {
+        font-size: 12px;
     }
 
     .bottom {
@@ -36,11 +49,12 @@
         bottom: 0;
     }
 
-    .left {
-        float: left;
+    .author {
+        margin-bottom: 0;
     }
 
     .reading-time {
+        margin-top: 0.2rem;
         color: #9B9B9B;
     }
 </style>
