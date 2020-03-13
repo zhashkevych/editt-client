ce64d50be97b9cde2d9f8e3898b2e1462b8409ca
Max Zhashkevych <maxzhashkevych@Maxs-MacBook-Air.local>
2020-03-13 09:11:51 +0200

    created fresh vue app; removed helloworld; added header and footer componenents and described header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer,
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2e2e2e;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="header">
+        <h1>editt.</h1>
+    </div>
+</template>
+
+<style>
+    .header {
+        overflow: hidden;
+        padding: 20px 10px;
+        text-align: left;
+    }
+
+    .header h1 {
+        font-size: 48px;
+        font-weight: bold;
+        padding-left: 2em;
+    }
+
+    @media screen and (max-width: 500px) {
+        .header {
+            text-align: center;
+        }
+        .header h1 {
+            padding: 0;
+        }
+    }
+</style>
