0bace25aa92fc42453cdffdd123096ef68fa79bc
zhashkevych <zhashkevychmaksim@gmail.com>
2020-03-22 16:59:08 +0200

    fixed sh script

--- run-container.sh
+++ run-container.sh
@@ -2,5 +2,6 @@ if [ "$(docker ps -q -f name=editt-client)" ]; then
     if [ ! "$(docker ps -aq -f status=exited -f name=editt-client)" ]; then
         docker stop editt-client
     fi
-    docker run --rm -d --publish 80:80 --network editt --name editt-client editt-client:0.1
 fi
+
+docker run --rm -d --publish 80:80 --network editt --name editt-client editt-client:0.1
